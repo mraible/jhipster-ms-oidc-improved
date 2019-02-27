8c47ec921282782fccc85d0d228ccceb2e64f76a
Matt Raible <matt.raible@okta.com>
2019-02-26 17:43:18 -0700

    Upgrade gateway to use Spring Security 5.1 OIDC

--- gateway/src/main/java/com/okta/developer/gateway/client/TokenRelayRequestInterceptor.java
+++ gateway/src/main/java/com/okta/developer/gateway/client/TokenRelayRequestInterceptor.java
@@ -21,8 +21,6 @@ public class TokenRelayRequestInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate template) {
         Optional<String> authorizationHeader = authorizationHeaderUtil.getAuthorizationHeaderFromOAuth2Context();
-        if (authorizationHeader.isPresent()) {
-            template.header(AUTHORIZATION, authorizationHeader.get());
-        }
+        authorizationHeader.ifPresent(s -> template.header(AUTHORIZATION, s));
     }
 }
